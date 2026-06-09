9b4e7494fbb00eb3ae3fd40388a9c8a4ba729ab4
sn45k58myn-dev <sn45k58myn@privaterelay.appleid.com>
2026-06-09 19:48:36 +0100

    Remove dead IPTV sources

--- src/sources/sourceRegistry.ts
+++ src/sources/sourceRegistry.ts
@@ -2,16 +2,22 @@ import { customXmltvUrls } from '../config/env';
 import { SourceDefinition } from '../models/xmltv';
 
 export const builtInSources: SourceDefinition[] = [
-  { name: 'epg.pw UK', type: 'epg.pw', url: 'https://epg.pw/xmltv/epg_GB.xml', priority: 10 },
-  { name: 'epg.pw US', type: 'epg.pw', url: 'https://epg.pw/xmltv/epg_US.xml', priority: 10 },
-  { name: 'IPTV-Org UK', type: 'iptv-org', url: 'https://iptv-org.github.io/epg/guides/uk.xml', priority: 20 },
-  { name: 'IPTV-Org US', type: 'iptv-org', url: 'https://iptv-org.github.io/epg/guides/us.xml', priority: 20 },
-  { name: 'Schedules Direct', type: 'schedules-direct', priority: 5 }
+  {
+    name: 'epg.pw UK',
+    type: 'epg.pw',
+    url: 'https://epg.pw/xmltv/epg_GB.xml',
+    priority: 10
+  }
 ];
 
 export function getConfiguredSources(): SourceDefinition[] {
   return [
     ...builtInSources,
-    ...customXmltvUrls.map((url, index) => ({ name: `Custom XMLTV URL ${index + 1}`, type: 'custom-url' as const, url, priority: 50 }))
+    ...customXmltvUrls.map((url, index) => ({
+      name: `Custom XMLTV URL ${index + 1}`,
+      type: 'custom-url' as const,
+      url,
+      priority: 50
+    }))
   ];
 }
